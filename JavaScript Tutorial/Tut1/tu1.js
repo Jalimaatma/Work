2b4238fa0e674937d19d74179d193c2f5d7c4f48
Shrikant <shrikantkus@gmail.com>
2023-04-28 21:58:53 +0530

    This is the message

---
+++ JavaScript Tutorial/Tut1/tu1.js
@@ -0,0 +1,39 @@
+// console.log("Hellow world");
+
+// function combine (firstname, Surname){
+//     alert(firstname + " " + Surname);
+// }
+
+//     for (i=0; i<2; i++)
+// {
+//     combine("Manish", "Jain")||
+//     combine("Hemant", "Goyal");
+
+// }
+// Question no 1
+let a= "Harry"
+let b= 7
+console.log(a+b);
+// Question no 2
+console.log(typeof (a+b));
+// Question no 3
+const a1 = {
+    Name: "Harry",
+    Surname: "Tiwari"
+    
+}
+console.log(a1);
+// Question no 4
+
+a1["Class"]="B.Sc."
+console.log(a1);
+
+// Question no 5
+
+const dict = {
+    sui_juri: "capable of managing one's affairs or assuming legal responsibility.",
+    farouche: "sullenly unsociable or shy.",
+    aurora:"a radiant emission from the upper atmosphere that occurs sporadically over the middle and high latitudes of both hemispheres in the form of luminous bands, streamers, or the like."
+
+}
+console.log(dict.aurora);
