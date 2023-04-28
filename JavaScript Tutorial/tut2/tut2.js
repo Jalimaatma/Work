bed3e0e57fdfd7cb5473fecd93879be27e8723ea
Shrikant <shrikantkus@gmail.com>
2023-04-28 22:55:19 +0530

    message

--- JavaScript Tutorial/tut2/tut2.js
+++ JavaScript Tutorial/tut2/tut2.js
@@ -1,4 +1,23 @@
-var age = prompt("What is your age????");
-var drive = age>=18&age<=70  ? "You can vote":"You can not vote";
-document.getElementById("heading").innerHTML= drive;
+// var age = prompt("What is your age????");
+// var drive = age>=18&age<=70  ? "You can vote":"You can not vote";
+// document.getElementById("heading").innerHTML= drive;
  
+
+const Time = ()=> {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth();
+    var date = now.getDate();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var secondes = now.getSeconds();
+
+    var todaysdate = date + "-" +month +"-" + year ;
+    var currenttime = hours+ ":"+minutes+":"+ secondes ;
+
+    document.getElementById("time").innerHTML = currenttime;
+    document.getElementById("date").innerHTML = todaysdate;
+
+}
+
+setInterval(Time, 1000);
